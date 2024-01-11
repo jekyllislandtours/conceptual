73b9f81803c3736cafdf2a651fd5805dd3036407
Amar Mehta <xfthhxk@gmail.com>
2024-01-11 13:11:19 -0500

    update build.clj to include required license entries in POM

--- build.clj
+++ build.clj
@@ -1,26 +1,30 @@
 (ns build
   (:require [clojure.tools.build.api :as b]
-            [org.corfield.build :as bb]
+            [deps-deploy.deps-deploy :as dd]
             [clojure.pprint :as pprint]))
 
 (def lib 'org.clojars.jekyllislandtours/conceptual)
 (def version (format "0.1.%s" (b/git-count-revs nil)))
+(def class-dir "target/classes")
+(def jar-file (format "target/%s-%s.jar" (name lib) version))
+
+(def basis (delay (b/create-basis {:project "deps.edn"})))
 
 (defn show-defaults [_]
   (println "default-basis:")
-  (pprint/pprint (bb/default-basis))
-  (println "default-target:" (bb/default-target))
-  (println "default-class-dir:" (bb/default-class-dir))
-  (println "default-jar-file:" (bb/default-jar-file lib version)))
+  (pprint/pprint @basis)
+  (println "target:" "target")
+  (println "class-dir:" class-dir)
+  (println "jar-file:" jar-file))
 
 (defn clean [_]
-  (bb/clean {}))
+  (b/delete {:path "target"}))
 
 (defn compile-java [_]
   (println "Compiling java...")
   (b/javac {:src-dirs ["src/java"]
-            :class-dir (bb/default-class-dir)
-            :basis (bb/default-basis)
+            :class-dir class-dir
+            :basis @basis
             :javac-opts ["--release" "11"
                          "-Xlint:deprecation" "-Xlint:unchecked"
                          "-proc:none"]}))
@@ -29,24 +33,49 @@
 (defn compile-clj [_]
   (println "Compiling clj...")
   (b/compile-clj {:src-dirs ["src/clj"]
-                  :class-dir (bb/default-class-dir)
-                  :basis (bb/default-basis)}))
+                  :class-dir class-dir
+                  :basis @basis}))
+
+
+(defn- pom-template [version]
+  [[:description "Conceptual is a fast JVM based in-memory concept database that can be used as a feature store, graph database, and more."]
+   [:url "https://github.com/jekyllislandtours/conceptual"]
+   [:licenses
+    [:license
+     [:name "MIT License"]
+     [:url "https://opensource.org/license/mit/"]]]
+   [:developers
+    [:developer
+     [:name "Scott Jappinen"]]]
+   [:scm
+    [:url "https://github.com/jekyllislandtours/conceptual"]
+    [:connection "scm:git:https://github.com/jekyllislandtours/conceptual.git"]
+    [:developerConnection "scm:git:ssh:git@github.com:jekyllislandtours/conceptual.git"]
+    [:tag (str "v" version)]]])
 
 (defn jar [_]
   (clean nil)
   (compile-java nil)
-  (bb/jar {:lib lib
-           :version version
-           :src-dirs ["src/clj"]}))
+  (b/write-pom {:class-dir class-dir
+                :lib lib
+                :version version
+                :basis @basis
+                :src-dirs ["src/clj"]
+                :pom-data (pom-template version)})
+  (b/copy-dir {:src-dirs ["src" "resources"]
+               :target-dir class-dir})
+  (b/jar {:class-dir class-dir
+          :jar-file jar-file}))
 
 (defn install [_]
   (jar nil)
   (println "Installing jar into local Maven repo cache...")
-  (bb/install {:lib lib
-               :version version
-               :src-dirs ["src/clj"]}))
-
-(defn deploy [_]
-  (bb/deploy {:lib lib
+  (b/install {:lib lib
               :version version
               :src-dirs ["src/clj"]}))
+
+(defn deploy [_]
+  (dd/deploy {:installer :remote
+              :artifact (b/resolve-path jar-file)
+              :pom-file (b/pom-path {:lib lib
+                                     :class-dir class-dir})}))
