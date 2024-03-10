71e001899c20bc8c98333e2c42925a5ad02f7d1b
Amar Mehta <xfthhxk@gmail.com>
2024-03-10 16:05:12 -0400

    Fixes #51, better error reporting from map->kvs

--- test/clj/conceptual/core_test.clj
+++ test/clj/conceptual/core_test.clj
@@ -10,3 +10,15 @@
   (expect [0] (vec (c/normalize-ids [:db/id])))
   ;; Fixes issue https://github.com/jekyllislandtours/conceptual/issues/52
   (expect [0] (vec (c/normalize-ids (seq [:db/id])))))
+
+(deftest map-kvs-test
+  ;; Fixes issue https://github.com/jekyllislandtours/conceptual/issues/51
+  (let [f (fn [m] (#'c/map->kvs (c/db) m))
+        try-f (fn [m]
+                (try
+                  (f m)
+                  (catch Exception ex
+                    (some-> ex ex-data))))]
+    (expect [[0] [1]] (mapv vec (f {:db/id 1})))
+    (expect [[0 1] [1 :hello]] (mapv vec (f {:db/id 1 :db/key :hello})))
+    (expect {:key :unknown/key} (try-f {:db/id 1 :unknown/key :hello}))))
