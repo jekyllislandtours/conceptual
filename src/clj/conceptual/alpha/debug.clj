76d089b92337c95547b7724e7316b720477c43cc
Amar Mehta <xfthhxk@users.noreply.github.com>
2024-05-14 20:17:59 -0400

    debug schema mismatches (#76)

--- src/clj/conceptual/alpha/debug.clj
+++ src/clj/conceptual/alpha/debug.clj
@@ -3,8 +3,22 @@
             [conceptual.int-sets :as i]
             [clojure.data :as data]))
 
+(defn known-keys
+  "Returns all known keys that are not of the `:db` namespace."
+  ([]
+   (known-keys (c/db)))
+  ([db]
+   (known-keys db [:db/tag? :db/property? :db/relation?]))
+  ([db tags]
+   (->> tags
+        (map c/ids)
+        (apply i/union)
+        (map (partial c/id->key db))
+        (remove #(= "db" (namespace %)))
+        set)))
+
 (defn unknown-keys
-  "Returns `nil` if no unknown keys are found else a map with keys `:pickle-only-keys`
+  "Returns `nil` if no unknown keys are found else a map with keys `:conceptual-only-keys`
   and `:input-only-keys`. You probably want to call this immediately after loading
   a pickle. `tag` is a tag such as `:db/tag?`or `:db/property?` or `:db/relation?`.
   `ks` is a seq of keywords that are expected to be tagged with `tag`."
@@ -13,14 +27,8 @@
   ([db ks]
    (unknown-keys db ks [:db/tag? :db/property? :db/relation?]))
   ([db ks tags]
-   (let [found (->> tags
-                    (map c/ids)
-                    (apply i/union)
-                    (map (partial c/id->key db))
-                    (remove #(= "db" (namespace %)))
-                    set)
-         [pickle-only input-only _] (data/diff found (set ks))]
-     (when (or (seq pickle-only)
+   (let [[conceptual-only input-only _] (data/diff (known-keys db tags) (set ks))]
+     (when (or (seq conceptual-only)
                (seq input-only))
-       {:pickle-only-keys (into (sorted-set) pickle-only)
+       {:conceptual-only-keys (into (sorted-set) conceptual-only)
         :input-only-keys (into (sorted-set) input-only)}))))
