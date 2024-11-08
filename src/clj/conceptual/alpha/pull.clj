fad5c61585ccc40a713285f053b78ef60a65eb9a
Amar Mehta <xfthhxk@gmail.com>
2024-11-07 20:19:50 -0500

    pull supports variables, tracks depth

--- src/clj/conceptual/alpha/pull.clj
+++ src/clj/conceptual/alpha/pull.clj
@@ -6,14 +6,11 @@
 
 ;; inspiration from https://docs.datomic.com/query/query-pull.html
 
-;; TODO:
-;; - wildcard support
-;;
+(set! *warn-on-reflection* true)
 
 (def known-opts #{:limit :as :filter})
 
 (declare pull)
-
 
 (defn validate-limit
   [k limit]
@@ -32,20 +29,26 @@
                       {::error ::invalid-as-option-value
                        :pull/key k})))))
 
-(def default-key+opts (juxt :pull/key :pull/key-opts))
+(defn variable?
+  [x]
+  (and (symbol? x)
+       (-> x name (.charAt 0) (= \$))))
+
 
 (defn vector->key+opts
-  [{:keys [pull/key+opts]
-    :or {key+opts default-key+opts}:as ctx} [k & kv-pairs]]
+  [{:keys [pull/variable-value]} [k & kv-pairs]]
   (let [{:keys [limit as] :as opts} (if (even? (count kv-pairs))
                                       (apply hash-map kv-pairs)
                                       (throw (ex-info "Expected 0 or an even number of options for key"
                                                       {::error ::not-enough-input-for-options
                                                        :pull/key k
                                                        :pull/opts kv-pairs})))
-        f-sexp (:filter opts)
+        f-sexp|var (:filter opts)
+        f-sexp (if (variable? f-sexp|var)
+                 (get variable-value f-sexp|var)
+                 f-sexp|var)
         opts (cond-> opts
-               f-sexp (update :filter c.filter/conform))]
+               f-sexp (assoc :filter (c.filter/conform f-sexp)))]
     (when-let [unk-ks (->> (keys opts)
                            (remove known-opts)
                            set
@@ -56,21 +59,23 @@
                        :pull/unknown-opts unk-ks})))
     (validate-limit k limit)
     (validate-as k as)
-    (key+opts (cond-> {:pull/ctx ctx :pull/key k :pull/key-opts opts}
-                f-sexp (assoc :pull/filter f-sexp)))))
+    [k opts]))
+
 
 (defn parse
   [ctx pattern]
-  (let [f (fn [x]
-            (cond
-              (keyword? x) :kws
-              (vector? x) :kw+opts
-              (map? x) :relations))
-        {:keys [kws kw+opts relations]} (group-by f pattern)
-        kw+opts (mapv (partial vector->key+opts ctx) kw+opts)]
-    {:pull/ks (into (set kws) (map first kw+opts))
-     :pull/k+opts kw+opts
-     :pull/relations relations}))
+  (let [ks (java.util.ArrayList.)
+        rels (java.util.ArrayList.)]
+    (loop [[x & more] pattern]
+      (if-not x
+        {:pull/key+opts (vec ks)
+         :pull/relations (vec rels)}
+        (do
+          (cond
+            (keyword? x) (.add ks (vector->key+opts ctx [x]))
+            (vector? x) (.add ks (vector->key+opts ctx x))
+            (map? x) (.add rels x))
+          (recur more))))))
 
 (defn apply-key-opts
   [c [k {:keys [limit as]}]]
@@ -128,6 +133,7 @@
                         :pull/key k
                         :pull/key-opts k-opts
                         :pull/concept c
+                        :pull/depth (::depth ctx)
                         :db/id (:db/id c)}
               id+ (relation-value rel-opts)
               ids (if (int? id+) id+ (i/set id+))
@@ -143,60 +149,79 @@
 
 (defn reify-relations
   [ctx relations c]
-  (loop [c c
-         [r & more] relations]
-    (if-not r
-      c
-      (recur (reify-relations* ctx r c) more))))
+  (let [ctx (update ctx ::depth (fnil inc 0))]
+    (loop [c c
+           [r & more] relations]
+      (if-not r
+        c
+        (recur (reify-relations* ctx r c) more)))))
 
 
 (defn map-invert+
   [m]
   (persistent!
    (reduce-kv (fn [ans k v]
-             (let [ks (ans v #{})]
-               (assoc! ans v (conj ks k))))
-           (transient {})
-           m)))
+                (let [ks (ans v #{})]
+                  (assoc! ans v (conj ks k))))
+              (transient {})
+              m)))
 
 (def default-finalizer :pull/concept)
+
+(defn default-pattern-finalizer
+  [{:keys [pull/key+opts pull/relations]}]
+  {:pull/key+opts key+opts
+   :pull/relations relations})
 
 (defn pull
   "`id+` can be an `int`, `int-array` or any seq of int.
   `ctx` is a map and can include fns to customize behavior. All the fns
   receive a map with key `:pull/ctx`.
 
-  `:pull/key+opts`
-     - fn which takes in a map and returns a tuple of keyword and opts map [key opts]
-     - returned opts map can have keys such as `:limit`,
-     - input map has keys `:pull/ctx`, `:pull/key` and `:pull/key-opts`
+  `:pull/pattern-finalizer`
+     - fn which takes in a map and returns a map with keys `:pull/key+opts` and `:pull/relations`
+     - input map has keys `:pull/ctx`, `:pull/key+opts` and `:pull/relations`
+     - opportunity to do any validation checks or transformations
+
+  `:pull/variable-value`
+     - a map of symbol variables to values. variables must begin with a `$` char
+     - only the `:filter` option on an expanded relation supports variables
 
   `:pull/relation-value`
      - fn which takes in a map and returns either a conceptual db/id or a sequence or int array of db/ids
-     - input map has keys `:pull/ctx`, `:pull/key`, `:pull/key-opts`, `:pull/concept` and `:db/id`
+     - input map has keys `:pull/ctx`, `:pull/key`, `:pull/key-opts`, `:pull/concept`, `:pull/depth` and `:db/id`
      - `:pull/concept` is the current concept being built up
+     - `:pull/depth` is a zero based integer indicating the depth of the graph at this point
+     - an opportunity to enforce depth and return relation values based on authorization etc
 
   `:pull/relation-finalizer`
      - fn which takes in a map and returns either a conceptual db/id or a sequence or int array of db/ids
      - input map has keys `:pull/ctx`, `:pull/key`, `:pull/key-opts`, `:pull/concept`, `:db/id` and
-       optionally `:pull/pre-limit-ids`
+       optionally `:pull/pre-limit-ids`.
      - `:pull/concept` is the current concept being built up
+     - an opportunity to associate a metadata ie to convey total count when limit is used, etc
+     - returns the concept
 
   `:pull/concept-finalizer`
      - fn for apply custom business logic to a concept before it is added to output. Returns the concept.
      - input map has keys `:pull/ctx`, `:pull/as->k`, `:pull/k->as`, and `:pull/concept`
      - `:pull/concept` is the current concept being built up"
-  [{:keys [pull/concept-finalizer] :or {concept-finalizer default-finalizer} :as ctx} pattern id+]
-  (let [{:keys [pull/ks pull/k+opts pull/relations] :as _parsed} (parse ctx pattern)
+  [{:keys [pull/concept-finalizer
+           pull/pattern-finalizer] :or {concept-finalizer default-finalizer
+                                        pattern-finalizer default-pattern-finalizer} :as ctx} pattern id+]
+  (let [{:keys [pull/key+opts pull/relations]} (-> (parse ctx pattern)
+                                                   (assoc :pull/ctx ctx)
+                                                   pattern-finalizer)
+        ks (set (map first key+opts))
         rm-db-id? (not (contains? ks :db/id))
-        as->k (into {} (for [[k {:keys [as]}] k+opts
+        as->k (into {} (for [[k {:keys [as]}] key+opts
                              :when as]
                          [as k]))
         k->as (map-invert+ as->k)
         cb-opts {:pull/ctx ctx
                  :pull/as->k as->k
                  :pull/k->as k->as}
-        xform (comp (map (partial apply-all-opts k+opts))
+        xform (comp (map (partial apply-all-opts key+opts))
                  (map (partial reify-relations ctx relations))
                  (map #(concept-finalizer (assoc cb-opts :pull/concept %)))
                  (map (fn [c]
