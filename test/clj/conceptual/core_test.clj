4bef28ad0dddf6654e9d1d228a9b597495ac903e
Amar Mehta <xfthhxk@users.noreply.github.com>
2025-06-02 16:37:18 -0400

    Fixes #91 and #109, address NPE in c/normalize-ids and pull patterns (#113)

--- test/clj/conceptual/core_test.clj
+++ test/clj/conceptual/core_test.clj
@@ -1,15 +1,24 @@
 (ns conceptual.core-test
   (:require [conceptual.core :as c]
-            [clojure.test :refer [deftest use-fixtures]]
+            [clojure.test :refer [deftest use-fixtures testing]]
             [conceptual.test.core :as test.core]
             [expectations.clojure.test :refer [expect]]))
 
 (use-fixtures :each test.core/with-rdb)
 
+(deftest keys->ids-test
+  (testing "unknown keys are ignored, data is sorted and unique"
+    (expect (vec (sort (map c/key->id [:sf/id :db/id :sf/name])))
+            (vec (c/keys->ids [:db/id :sf/id :sf/id :undefined/field-kd83jj :sf/name :not-a-field])))))
+
 (deftest normalize-ids-test
   (expect [0] (vec (c/normalize-ids [:db/id])))
   ;; Fixes issue https://github.com/jekyllislandtours/conceptual/issues/52
-  (expect [0] (vec (c/normalize-ids (seq [:db/id])))))
+  (expect [0] (vec (c/normalize-ids (seq [:db/id]))))
+
+  (testing "unknown keys are ignored, data is not sorted and duplicates are allowed"
+    (expect (mapv c/key->id [:db/id :sf/id :sf/name :sf/id])
+            (vec (c/normalize-ids [:db/id :sf/id :undefined/field-kd83jj :sf/name :not-a-field :sf/id])))))
 
 (deftest map-kvs-test
   ;; Fixes issue https://github.com/jekyllislandtours/conceptual/issues/51
